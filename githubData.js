7a3d417955dfb9e1b09835fab03d5c55a7f3f634
LachlanStephan <ljstephan116@gmail.com>
2022-01-06 10:51:48 +1000

    projects being fetched and set in html

--- githubData.js
+++ githubData.js
@@ -15,31 +15,55 @@ const gHubData = async () => {
 			method: "GET",
 			auth: auth,
 		});
+		console.log(res.data);
 		projArray.push(res.data);
 	}
 	setProjects();
 };
-gHubData();
+
+const doGhubData = async () => {
+	try {
+		gHubData();
+	} catch (e) {
+		console.log(e);
+	}
+};
+doGhubData();
 
 const setProjects = () => {
-	const descPrefix = "Description: ";
-
+	// const descPrefix = "Description: ";
 	const sect = document.getElementById("sect");
 	for (let i = 0; i < projArray.length; i++) {
 		// HTML elements
 		const container = document.createElement("div");
 		const heading = document.createElement("h4");
 		const desc = document.createElement("p");
+		const url = document.createElement("a");
+
+		// Add class to proj container
+		container.classList.add("projContainer");
 
 		// Create container for each
 		sect.appendChild(container);
 		container.appendChild(heading);
 
 		// Heading text
-		heading.innerHTML = projArray[i].name;
+		heading.innerHTML = projArray[i].name + ":";
 
 		// Description text
-		desc.innerHTML = descPrefix + projArray[i].description;
+		desc.innerHTML = projArray[i].description;
 		heading.after(desc);
+
+		// Add url
+		url.href = projArray[i].html_url;
+		url.innerHTML = "link";
+		desc.after(url);
+
+		if (
+			projArray[i].description == null ||
+			projArray[i].description == ""
+		) {
+			desc.innerHTML = "No description";
+		}
 	}
 };
