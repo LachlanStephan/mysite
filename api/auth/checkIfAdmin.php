a591fd831721a3664d82f49a240db96d50f13245
Lachlan Stephan <ljstephan116@gmail.com>
2022-03-25 20:10:01 +1000

    stuffs

--- api/auth/checkIfAdmin.php
+++ api/auth/checkIfAdmin.php
@@ -1,38 +1,17 @@
 <?php
     require("../index.php");
+    require("./functions.php");
 
     $res = [
         'status' => 403,
-        'msg' => '',
+        'msg' => 'invalid',
     ];
-
-    function validatePass($pass) {
-        global $conn;
-
-        $u = 'Lach';
-        $sql = '
-            SELECT user_name, password 
-            FROM user 
-            WHERE user_name = :u
-        ';
-
-        $stmt = $conn->prepare($sql);
-        $stmt->execute(array(
-            ':u' => $u
-        ));
-
-        $row = $stmt->fetch();
-        if ($row['password'] === $pass) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 
     if (!empty($_POST)) {
         $pass = $_POST['pass'];
         if (validatePass($pass)) {
             $res['status'] = 202;
+            $res['msg'] = 'valid';
         }
     }
 
