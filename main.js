d43d4829cf76aac683dc02fb0d6526bfeedb0a23
LachlanStephan <ljstephan116@gmail.com>
2022-05-10 21:22:12 +1000

    refactor to class - main.js

--- main.js
+++ main.js
@@ -1,24 +1,37 @@
-const fetchSiteData = async () => {
-	await Promise.all([fetchSections(), getBlogs()]);
-};
+class Main {
+	// private properties
+	#body;
+	#curr_theme;
 
-const checkTheme = () => {
-	const body = document.getElementById("body");
-	const curr_theme = localStorage.getItem("current_mode");
-	if (curr_theme !== null) {
-		body.className = curr_theme;
-		setCorrectIcon(curr_theme);
+	// initialise values
+	constructor() {
+		this.#body = document.getElementById("body");
+		this.#curr_theme = localStorage.getItem("current_mode");
 	}
-};
 
-const setCorrectIcon = (curr_theme) => {
-	console.warn(curr_theme);
-	if (curr_theme === "lightMode") {
-		toggleIcon("off");
-	} else {
-		toggleIcon("on");
+	// private methods
+	#setCorrectIcon() {
+		if (this.#curr_theme === "lightMode") {
+			toggleIcon("off");
+		} else {
+			toggleIcon("on");
+		}
 	}
-};
 
-checkTheme();
-fetchSiteData();
+	// public methods
+	async fetchSiteData() {
+		await Promise.all([fetchSections(), getBlogs()]);
+	}
+
+	checkTheme() {
+		if (this.#curr_theme !== null) {
+			this.#body.className = this.#curr_theme;
+			this.#setCorrectIcon();
+		}
+	}
+}
+
+const m = new Main();
+
+m.checkTheme();
+m.fetchSiteData();
