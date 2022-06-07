1b3d8ed192ad9b9b31576d9c23283a30547b2c1b
Lachlan Stephan <ljstephan116@gmail.com>
2022-06-07 23:19:50 +1000

    commit board working

--- components/commitBoard.tsx
+++ components/commitBoard.tsx
@@ -1,34 +1,59 @@
 import { FC, useState, useEffect } from "react";
+import { format } from "date-fns";
 
 interface Props {
-    eventsTotal: any[];
+	eventsTotal: any[];
 }
 
 export const CommitBoard: FC<Props> = (Props) => {
-    const squares = Props.eventsTotal;
+	const commits = Props.eventsTotal;
+	let boardArray: string[] = [];
 
-    // TODO:
-    //  how to get 30 last days to show
-    //  sort by date - need to format dates?
-    //  sort colour by psuhEvents per date
-    //  
-    const board = squares.map((v: any, i: any) => {
-        const currDate = new Date();
-        console.log(currDate, "CURRENT");
-        if (v.type === "PushEvent") {
-            console.log(v.created_at, "COMMITED AT");
-            return <div key={i} className="h-2 w-2 bg-green-600"></div>;
-        }
-        return <div key={i} className="h-2 w-2 bg-black"></div>;
-    });
+	const formatCreatedAt = (created_at: string): string => {
+		const splitDate: string[] = created_at.split("-");
+		const year: string = splitDate[0];
+		const month: string = splitDate[1];
+		const day: string = splitDate[2].substring(0, 2);
+		return year + "-" + month + "-" + day;
+	};
 
-    return (
-        <>
-            <h1 className="font-bold">Github:</h1>
-            <br />
-            <div className="grid gap-1 grid-rows-10 grid-cols-10  h-auto w-auto">
-                {board}
-            </div>
-        </>
-    );
+	let count: number = 30;
+	const d: Date = new Date();
+	for (let i = 0; i < 30; i++) {
+		boardArray.push(
+			format(new Date().setDate(d.getDate() - count), "yyyy-MM-dd")
+		);
+		count = count - 1;
+	}
+
+	for (let i = 0; i < commits.length; i++) {
+		for (let j = 0; j < boardArray.length; j++) {
+			let x = formatCreatedAt(commits[i].created_at);
+			if (boardArray[j] == x && commits[i].type == "PushEvent") {
+				const el: HTMLElement | null = document.getElementById(
+					boardArray[j]
+				);
+				if (el != null) {
+					el.style.backgroundColor = "green";
+				}
+			}
+		}
+	}
+	return (
+		<>
+			<h1 className="font-bold">Github: Last 30 days</h1>
+			<br />
+			<div className="grid gap-1 grid-rows-5 grid-cols-6  h-auto w-auto">
+				{boardArray.map((v: string, i: number) => {
+					return (
+						<div
+							key={i}
+							id={v}
+							className="w-3 h-3 bg-black rounded-sm"
+						></div>
+					);
+				})}
+			</div>
+		</>
+	);
 };
