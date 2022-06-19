07f2743e91072a51219b4256256ff428a055c552
LachlanStephan <ljstephan116@gmail.com>
2022-06-19 21:38:45 +1000

    proper modal implemented

--- components/projects/info.tsx
+++ components/projects/info.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import Markdown from 'markdown-to-jsx'
+import Markdown from "markdown-to-jsx";
 import { FaRegWindowClose } from "react-icons/fa";
 
 interface Data {
@@ -12,24 +12,34 @@ interface Props {
   data: Data;
 }
 
-// TODO: 
+// TODO:
 // should do err handling on the fetched data - failed || empty
-// need to make a proper modal without pos absolute
 
 export const Info: FC<Props> = (Props) => {
   const data = Props;
   return (
     <>
-      <div className="h-3/6 w-3/5 xs:w-full lg:w-2/5 absolute bg-white border-2 rounded-lg flex flex-col p-2 inset-x-1/4 inset-y-32 overflow-y-auto overflow-x-clip">
-        <div className="flex h-10 w-full justify-between">{data.data.name}
-          <div className="cursor-pointer" onClick={() => Props.closeModal()}>
-            <FaRegWindowClose />
+      <div className="flex justify-center items-start overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
+        <div className="p-4 w-full max-w-2xl h-full md:h-auto">
+          <div className="bg-white border-2 border-gray-700 rounded-lg shadow">
+            <div className="flex justify-between items-start p-4 t border-b">
+              <h3 className="text-xl font-semibold">
+                {data.data.name}
+              </h3>
+              <button
+                onClick={() => Props.closeModal()}
+                type="button"
+                className="text-gray-400 hover:text-gray-900 text-sm"
+              >
+                <FaRegWindowClose />
+              </button>
+            </div>
+            <div className="p-6 space-y-6">
+              <div className="break-all max-h-96 overflow-auto">
+                <Markdown>{atob(data.data.content)}</Markdown>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="break-all">
-        <Markdown>
-          {atob(data.data.content)}
-        </Markdown>
         </div>
       </div>
     </>
