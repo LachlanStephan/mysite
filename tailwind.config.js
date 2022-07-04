3622e2a777449a3976d92339377ae2bf3a401ac5
Lachlan Stephan <ljstephan116@gmail.com>
2022-07-04 21:49:39 +1000

    fix for width - hello str

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-  darkMode: "class",
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      animation: {
-        typing: "typing 1s steps(20, end) forwards",
-      },
-      keyframes: {
-        typing: {
-          from: {width: 0},
-          to: {width: 120},
-        },
-      },
-    },
-  },
-  plugins: [],
+	darkMode: "class",
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			animation: {
+				typing: "typing 1s steps(20, end) forwards",
+			},
+			keyframes: {
+				typing: {
+					from: { width: 0 },
+					to: { width: 75 },
+				},
+			},
+		},
+	},
+	plugins: [],
 };
