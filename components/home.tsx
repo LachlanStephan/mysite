c8b8c47c66f9a1fcfaaba6592d418781999160a6
LachlanStephan <ljstephan116@gmail.com>
2022-07-09 08:30:08 +1000

    add head meta data to home page

--- components/home.tsx
+++ components/home.tsx
@@ -1,10 +1,9 @@
-import { FC, useEffect } from "react";
+import { FC } from "react";
 // import { Texts } from "../components/texts";
 // import { textContent } from "../components/textsContent";
 import { CommitChart } from "../components/commitChart";
 import { Interests } from "../components/interests";
 import { About } from "../components/about";
-import Layout from "../components/layout";
 import Head from "next/head";
 
 // interface textStuff {
@@ -13,28 +12,29 @@ import Head from "next/head";
 // }
 
 export const Home: FC = () => {
-	// const texts: textStuff[] = textContent;
+  // const texts: textStuff[] = textContent;
 
-	// const textsList = texts.map((val, index) => {
-	// return <Texts key={index} title={val.title} text={val.content} />;
-	// });
+  // const textsList = texts.map((val, index) => {
+  // return <Texts key={index} title={val.title} text={val.content} />;
+  // });
 
-	// Implement if I want about sections
-	// <h2 className="text-lg font-bold">About</h2>
-	// <br />
-	// {textsList}
+  // Implement if I want about sections
+  // <h2 className="text-lg font-bold">About</h2>
+  // <br />
+  // {textsList}
 
-	return (
-		<Layout>
-			<Head>
-				<title>Home - Lachlan Stephan</title>
-			</Head>
-				<About
-					name="Lachlan Stephan"
-					desc="Junior Software Dev - trying to be a somewhat decent developer..."
-				/>
-				<Interests title="Current interests" />
-				<CommitChart />
-		</Layout>
-	);
+  return (
+    <>
+      <Head>
+        <title>Home - Lachlan Stephan</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <About
+        name="Lachlan Stephan"
+        desc="Junior Software Dev - trying to be a somewhat decent developer..."
+      />
+      <Interests title="Current interests" />
+      <CommitChart />
+    </>
+  );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import type { ReactElement } from "react";
 import { Home } from "../components/home";
+import Layout from "../components/layout";
 
 export default function Page() {
-	return <Home />;
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  );
 }
 
 Page.getLayout = function getLayout(page: ReactElement) {
-	return <>{page}</>;
+  return <>{page}</>;
 };
