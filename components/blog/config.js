9f328ee1fc4e197f184c936695a9fbae9d9d33a2
LachlanStephan <ljstephan116@gmail.com>
2022-07-30 21:42:18 +1000

    update packages - finish off mvp for new blog

--- components/blog/config.js
+++ components/blog/config.js
@@ -1,5 +1,10 @@
 export const Config = [
 	{
+		title: "Learning better languages",
+		link: "blogs/learning-better-languages",
+    tags: "opinion technical",
+	},
+	{
 		title: "One year in software",
 		link: "blogs/one-year-in-software",
     tags: "opinion story",
