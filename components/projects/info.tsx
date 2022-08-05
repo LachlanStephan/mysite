84822b07ef952619d52c803539d6ea56ee208bea
LachlanStephan <ljstephan116@gmail.com>
2022-08-05 19:47:21 +1000

    added handling for if site does not have a link/homepage url

--- components/projects/info.tsx
+++ components/projects/info.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
 import Markdown from "markdown-to-jsx";
 import { FaRegWindowClose } from "react-icons/fa";
 import Link from "next/link";
@@ -17,15 +17,25 @@ interface Props {
 
 export const Info: FC<Props> = (Props) => {
   const data = Props;
+  const [isHomepage, setIsHomepage] = useState(false);
+
+  const checkIfHomepage = () => {
+    if (data.homepage) {
+      setIsHomepage(true);
+    }
+  };
+
+  useEffect(() => {
+    checkIfHomepage();
+  }, []);
+
   return (
     <>
       <div className="flex justify-center items-start overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
         <div className="p-4 w-full max-w-2xl h-full md:h-auto">
           <div className="bg-light_background dark:bg-dark_background  border-2 rounded-lg shadow border-accent_grey">
             <div className="flex justify-between items-start p-6 t">
-              <h3 className="text-xl font-semibold">
-                {data.data.name}
-              </h3>
+              <h3 className="text-xl font-semibold">{data.data.name}</h3>
               <button
                 onClick={() => Props.closeModal()}
                 type="button"
@@ -34,9 +44,15 @@ export const Info: FC<Props> = (Props) => {
                 <FaRegWindowClose />
               </button>
             </div>
-            <Link href={data.homepage}>
-              <a className="ml-6" target="_blank">To site</a>
-            </Link>
+            {isHomepage ? (
+              <Link href={data.homepage}>
+                <a className="ml-6" target="_blank">
+                  To site
+                </a>
+              </Link>
+            ) : (
+              null
+            )}
             <div className="p-6 space-y-6">
               <div className="break-all max-h-96 overflow-auto">
                 <Markdown>{atob(data.data.content)}</Markdown>
